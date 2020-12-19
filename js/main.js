0569be34c023f8962301a78bae9369ad0d71b9db
Laura <laurasarabiaf@gmail.com>
2020-12-18 21:38:22 -0300

    cambios nuevos

--- js/main.js
+++ js/main.js
@@ -6,67 +6,49 @@ function validarEmail(valor) {
   }
 }
 
-
 function validateForm(){
 	/* Escribe tú código aquí */
 	//alert ("funciona");
 	$(".form-signup").submit(function(){	
-	var nombre= $("#name").val();
-	if (nombre == ""){
-		$("#name").parent().append("<span>Ingresar Nombre</span>");
-		return false;
-	}
-	else if (nombre == numeros){
-		$("#name").parent().append("<span>Utilizar solo letras</span>");
-		return false;
+		var nombre= $("#name").val();
+		if (nombre == ""){
+			$("#name").parent().append("<span>Ingresar Nombre</span>");
+			return false;
 		}
-	}      
-		}
-	}      
-		}
-	}      
-		}
+		else if (/^[a-zA-Z]+$/.test(nombre) == false){
+			$("#name").parent().append("<span>Utilizar solo letras</span>");
+			return false;
+		}     
 	});
 
 	//apellido
 	var apellido= $("#lastname").val();
-		if (apellido == ""){
-			$("#lastname").parent().append("<span>Ingresar Apellido</span>");
-			return false;
-		}
+	if (apellido == ""){
+		$("#lastname").parent().append("<span>Ingresar Apellido</span>");
+	}
 
-		else if (/^[a-zA-Z]+$/.test(apellido) == false){
-			$("#lastname").parent().append("<span>Ingresar solo letras</span>");			
-			return true;
-		}
+	else if (/^[a-zA-Z]+$/.test(apellido) == false){
+		$("#lastname").parent().append("<span>Ingresar solo letras</span>");			
+	}
 
 		//password
-		var password= $("#input-password").val();
-		if (password == ""){
-			$("#input-password").parent().append("<span>Ingresar Contraseña</span>");
-			return false;
-		}
+	var password= $("#input-password").val();
+	if (password == ""){
+		$("#input-password").parent().append("<span>Ingresar Contraseña</span>");
+	}
 	else if(password == "123456"){
 		$("#input-password").parent().append("<span>No puedes utilizar esta contraseña.Es vulnerable</span>");
-		return false;
-		}		
+	}		
+	else if(password.length < 6){
+		$("#input-password").parent().append("<span>La contraseña es muy corta</span>");
 	}
-		}		
+
+	//OPCIONES
+	var select= $("select").val();
+	if (select == 0){
+		$("select").parent().append("<span>Debes seleccionar una opcion</span>");
 	}
-		else if(password.length < 6){
-			$("#input-password").parent().append("<span>La contraseña es muy corta</span>");
-			return false;
-		}
 
-		//OPCIONES
-		var select= $("select").val();
-		if (select == 0){
-			$("select").parent().append("<span>Debes seleccionar una opcion</span>");
-			return false;
-		}
-
-
-	});
 		
 		//pattern="[A-Za-z]{4-16}"
 
@@ -82,7 +64,6 @@ function validateForm(){
 				//arreglar el formato
 		}
 	});
-
 }
 //hasta aqui todo bien
 //falta la 2
