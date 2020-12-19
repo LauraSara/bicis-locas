0d99f3113a1b2aca3247513b8a99a605e3ad6cb0
Laura <laurasarabiaf@gmail.com>
2020-12-18 22:17:29 -0300

    avances guardados

--- js/main.js
+++ js/main.js
@@ -18,29 +18,38 @@ function validateForm(){
 		else if (/^[a-zA-Z]+$/.test(nombre) == false){
 			$("#name").parent().append("<span>Utilizar solo letras</span>");
 			return false;
-		}     
-	});
+		}
+		else if (/^[A-Z]+$/.test(nombre[0]) == false){
+			$("#name").parent().append("<span>La inicial debe ser mayúscula</span>");
+			return false;
+		}		
+
+		});
 
 	//apellido
 	var apellido= $("#lastname").val();
 	if (apellido == ""){
 		$("#lastname").parent().append("<span>Ingresar Apellido</span>");
 	}
-
 	else if (/^[a-zA-Z]+$/.test(apellido) == false){
 		$("#lastname").parent().append("<span>Ingresar solo letras</span>");			
 	}
+	else if (/^[A-Z]+$/.test(apellido[0]) == false){
+		$("#lastname").parent().append("<span>La inicial debe ser mayúscula</span>");
+		return false;
+	}	
 
 		//password
 	var password= $("#input-password").val();
 	if (password == ""){
 		$("#input-password").parent().append("<span>Ingresar Contraseña</span>");
 	}
-	else if(password == "123456"){
+	else if(password == "123456" || password == "098765" || password == "password"){
 		$("#input-password").parent().append("<span>No puedes utilizar esta contraseña.Es vulnerable</span>");
 	}		
 	else if(password.length < 6){
 		$("#input-password").parent().append("<span>La contraseña es muy corta</span>");
+		return false
 	}
 
 	//OPCIONES
@@ -48,7 +57,6 @@ function validateForm(){
 	if (select == 0){
 		$("select").parent().append("<span>Debes seleccionar una opcion</span>");
 	}
-
 		
 		//pattern="[A-Za-z]{4-16}"
 
@@ -65,9 +73,6 @@ function validateForm(){
 		}
 	});
 }
-//hasta aqui todo bien
-//falta la 2
-//falta la 3
 
 
 
